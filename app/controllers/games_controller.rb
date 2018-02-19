36235c4835a0c2fae7bf8520d4dd62ef21934df6
khristoph <christopherfriedman@gmail.com>
2018-02-19 10:40:05 -0600

    Adds new and create actions in view files.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,20 @@
 class GamesController < ApplicationController
 
   def index
-
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new()
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :description, :creator)
+  end
+
 end
