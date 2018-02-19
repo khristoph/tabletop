67f0d165e27bd8769e557ccc08986dbd9ba1204a
khristoph <christopherfriedman@gmail.com>
2018-02-19 15:35:05 -0600

    Review model, migrations and associations.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :reviews
 
   has_attached_file :game_img, styles: { game_index: "250x350>", game_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :game_img, content_type: /\Aimage\/.*\z/
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219201717) do
+ActiveRecord::Schema.define(version: 20180219213203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20180219201717) do
     t.string "game_img_content_type"
     t.integer "game_img_file_size"
     t.datetime "game_img_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "game_id"
   end
 
   create_table "users", force: :cascade do |t|
