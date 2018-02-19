0cc777c7d1c989e694c52089702acd9988eb41c9
khristoph <christopherfriedman@gmail.com>
2018-02-19 14:55:18 -0600

    Render images to Index page.

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :game_img, styles => { :game_index "250x350>", play_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :game_img, styles: { game_index: "250x350>", play_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :game_img, content_type: /\Aimage\/.*\z/
 end
