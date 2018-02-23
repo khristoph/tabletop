1399ea7cc3297408ea854b9703f81c826f474521
khristoph <christopherfriedman@gmail.com>
2018-02-22 21:26:36 -0600

    Devise parameters.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { registration: 'registrations' }
   root 'games#index'
   resources :games do
     resources :reviews
